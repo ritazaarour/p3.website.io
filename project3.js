ca3a4b97dc4de82cab28b9f0a65007a252f33fa3
Rita Zaarour <ritazaarour9@gmail.com>
2025-11-06 10:22:00 -0800

    Update project3.js

--- project3.js
+++ project3.js
@@ -1,7 +1,9 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+
 d3.json("Surface_Temp_Change_reduced.json").then(rawData => {
   console.log("Data loaded:", rawData.length, "records");
 
-  // onvert year string to number
+  // convert year string to number
   rawData.forEach(d => {
     d.Year = +d.Year;
   });
