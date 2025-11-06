550734f20d349a0079b2191d1d5338d9c0e606cb
Rita Zaarour <ritazaarour9@gmail.com>
2025-11-06 10:16:49 -0800

    chart fixes

--- project3.js
+++ project3.js
@@ -1,26 +1,24 @@
-d3.json("Surface_Temp_Change_reduced.json").then(data => {
-  data.forEach(d => {
+d3.json("Surface_Temp_Change_reduced.json").then(rawData => {
+  console.log("Data loaded:", rawData.length, "records");
+
+  // onvert year string to number
+  rawData.forEach(d => {
     d.Year = +d.Year;
-    d.Value = +d.Value;
   });
 
-  const continents = d3.groups(data, d => d.Continent)
-    .map(([continent, values]) => ({
-      continent,
-      values: d3.rollups(
-        values,
-        v => d3.mean(v, d => d.Value),
-        d => d.Year
-      ).map(([Year, Value]) => ({ Year, Value }))
-      .sort((a, b) => a.Year - b.Year)
-    }));
+  // group country by continent 
+  const grouped = d3.groups(rawData, d => d.Continent);
+  const continents = grouped.map(([continent, values]) => ({
+    continent,
+    values: d3.rollups(values,
+      v => d3.mean(v, d => d.Value),
+      d => d.Year
+    ).map(([Year, Value]) => ({ Year, Value }))
+     .sort((a, b) => a.Year - b.Year)
+  }));
 
-  drawChart(continents);
-});
-
-
-function drawChart(continents) {
-  const margin = { top: 50, right: 120, bottom: 40, left: 60 };
+  // chart
+  const margin = { top: 40, right: 100, bottom: 50, left: 60 };
   const width = 900 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
@@ -31,79 +29,93 @@ function drawChart(continents) {
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const allYears = d3.extent(continents.flatMap(c => c.values.map(v => v.Year)));
-  const allValues = d3.extent(continents.flatMap(c => c.values.map(v => v.Value)));
+  const x = d3.scaleLinear()
+    .domain(d3.extent(rawData, d => d.Year))
+    .range([0, width]);
 
-  const x = d3.scaleLinear().domain(allYears).range([0, width]);
-  const y = d3.scaleLinear().domain(allValues).nice().range([height, 0]);
-  const color = d3.scaleOrdinal(d3.schemeCategory10)
-                  .domain(continents.map(c => c.continent));
+  const y = d3.scaleLinear()
+    .domain(d3.extent(rawData, d => d.Value))
+    .nice()
+    .range([height, 0]);
 
-  svg.append("g").attr("transform", `translate(0,${height})`).call(d3.axisBottom(x).tickFormat(d3.format("d")));
+  const color = d3.scaleOrdinal(d3.schemeTableau10)
+    .domain(continents.map(c => c.continent));
+
+  // axes
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
   svg.append("g").call(d3.axisLeft(y));
 
-  svg.append("text")
-     .attr("x", width / 2)
-     .attr("y", -20)
-     .attr("text-anchor", "middle")
-     .attr("font-size", "16px")
-     .text("Change in Global Temperature by Continent");
-       const line = d3.line()
-       .x(d => x(d.Year))
-       .y(d => y(d.Value));
+  // line
+  const line = d3.line()
+    .x(d => x(d.Year))
+    .y(d => y(d.Value));
 
-  const continentGroup = svg.selectAll(".continent")
+  svg.selectAll(".continent-line")
     .data(continents)
     .enter()
-    .append("g")
-    .attr("class", "continent");
-
-  continentGroup.append("path")
+    .append("path")
+    .attr("class", "continent-line")
     .attr("fill", "none")
     .attr("stroke", d => color(d.continent))
     .attr("stroke-width", 2)
     .attr("d", d => line(d.values));
 
-  const tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0)
-    .style("position", "absolute")
-    .style("background", "white")
-    .style("border", "1px solid #ccc")
-    .style("padding", "5px");
+  const tooltip = d3.select("body").append("div").attr("class", "tooltip");
 
-  continentGroup.selectAll("circle")
-    .data(d => d.values.map(v => ({...v, continent: d.continent})))
-    .enter()
-    .append("circle")
-    .attr("cx", d => x(d.Year))
-    .attr("cy", d => y(d.Value))
-    .attr("r", 3)
-    .attr("fill", d => color(d.continent))
-    .on("mouseover", (event, d) => {
-      tooltip.transition().style("opacity", 1);
-      tooltip.html(`<b>${d.continent}</b><br>${d.Year}: ${d.Value.toFixed(2)}°C`)
-        .style("left", event.pageX + 8 + "px")
-        .style("top", event.pageY - 28 + "px");
-    })
-    .on("mouseout", () => tooltip.transition().style("opacity", 0));
+  // slider
+  const yearExtent = d3.extent(rawData, d => d.Year);
+  let currentYear = yearExtent[0];
 
-  const legend = svg.selectAll(".legend")
-    .data(continents)
-    .enter()
-    .append("g")
-    .attr("class", "legend")
-    .attr("transform", (d, i) => `translate(${width + 10},${i * 25})`);
+  const slider = d3.select("#slider")
+    .append("input")
+    .attr("type", "range")
+    .attr("min", yearExtent[0])
+    .attr("max", yearExtent[1])
+    .attr("value", currentYear)
+    .attr("step", 1)
+    .style("width", "500px")
+    .on("input", function() {
+      currentYear = +this.value;
+      updateChart(currentYear);
+    });
 
-  legend.append("rect")
-    .attr("width", 12)
-    .attr("height", 12)
-    .attr("fill", d => color(d.continent));
+  const yearLabel = d3.select("#slider")
+    .append("div")
+    .style("margin-top", "8px")
+    .style("font-weight", "bold")
+    .text(`Year: ${currentYear}`);
 
-  legend.append("text")
-    .attr("x", 18)
-    .attr("y", 10)
-    .text(d => d.continent);
-}
+  function updateChart(year) {
+    yearLabel.text(`Year: ${year}`);
 
-    
+    const filtered = continents.map(c => ({
+      continent: c.continent,
+      value: c.values.find(v => v.Year === year)
+    })).filter(d => d.value);
+
+    const points = svg.selectAll(".year-dot")
+      .data(filtered, d => d.continent);
+
+    points.exit().remove();
+
+    points.enter()
+      .append("circle")
+      .attr("class", "year-dot")
+      .merge(points)
+      .attr("cx", d => x(d.value.Year))
+      .attr("cy", d => y(d.value.Value))
+      .attr("r", 5)
+      .attr("fill", d => color(d.continent))
+      .on("mouseover", (event, d) => {
+        tooltip.transition().style("opacity", 1);
+        tooltip.html(`<b>${d.continent}</b><br>${year}: ${d.value.Value.toFixed(2)}°C`)
+          .style("left", event.pageX + 8 + "px")
+          .style("top", event.pageY - 28 + "px");
+      })
+      .on("mouseout", () => tooltip.transition().style("opacity", 0));
+  }
+
+  updateChart(currentYear);
+});
