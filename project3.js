a44094f3370a8c5546d7f118507eb31d5f1aa489
Rita Zaarour <ritazaarour9@gmail.com>
2025-11-06 10:25:56 -0800

    convert to bar chart

--- project3.js
+++ project3.js
@@ -8,7 +8,7 @@ d3.json("Surface_Temp_Change_reduced.json").then(rawData => {
     d.Year = +d.Year;
   });
 
-  // group country by continent 
+  // group country by continent
   const grouped = d3.groups(rawData, d => d.Continent);
   const continents = grouped.map(([continent, values]) => ({
     continent,
@@ -19,8 +19,12 @@ d3.json("Surface_Temp_Change_reduced.json").then(rawData => {
      .sort((a, b) => a.Year - b.Year)
   }));
 
+  const allYears = d3.extent(rawData, d => d.Year);
+  const allContinents = continents.map(c => c.continent);
+  const yearList = Array.from(new Set(rawData.map(d => d.Year))).sort(d3.ascending);
+
   // chart
-  const margin = { top: 40, right: 100, bottom: 50, left: 60 };
+  const margin = { top: 40, right: 40, bottom: 50, left: 70 };
   const width = 900 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
@@ -31,9 +35,10 @@ d3.json("Surface_Temp_Change_reduced.json").then(rawData => {
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const x = d3.scaleLinear()
-    .domain(d3.extent(rawData, d => d.Year))
-    .range([0, width]);
+  const x = d3.scaleBand()
+    .domain(allContinents)
+    .range([0, width])
+    .padding(0.3);
 
   const y = d3.scaleLinear()
     .domain(d3.extent(rawData, d => d.Value))
@@ -41,40 +46,31 @@ d3.json("Surface_Temp_Change_reduced.json").then(rawData => {
     .range([height, 0]);
 
   const color = d3.scaleOrdinal(d3.schemeTableau10)
-    .domain(continents.map(c => c.continent));
+    .domain(allContinents);
 
   // axes
   svg.append("g")
     .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+    .call(d3.axisBottom(x));
   svg.append("g").call(d3.axisLeft(y));
 
-  // line
-  const line = d3.line()
-    .x(d => x(d.Year))
-    .y(d => y(d.Value));
-
-  svg.selectAll(".continent-line")
-    .data(continents)
-    .enter()
-    .append("path")
-    .attr("class", "continent-line")
-    .attr("fill", "none")
-    .attr("stroke", d => color(d.continent))
-    .attr("stroke-width", 2)
-    .attr("d", d => line(d.values));
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", -10)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "16px")
+    .text("Average Temperature Change by Continent");
 
   const tooltip = d3.select("body").append("div").attr("class", "tooltip");
 
   // slider
-  const yearExtent = d3.extent(rawData, d => d.Year);
-  let currentYear = yearExtent[0];
+  let currentYear = allYears[0];
 
   const slider = d3.select("#slider")
     .append("input")
     .attr("type", "range")
-    .attr("min", yearExtent[0])
-    .attr("max", yearExtent[1])
+    .attr("min", allYears[0])
+    .attr("max", allYears[1])
     .attr("value", currentYear)
     .attr("step", 1)
     .style("width", "500px")
@@ -89,35 +85,47 @@ d3.json("Surface_Temp_Change_reduced.json").then(rawData => {
     .style("font-weight", "bold")
     .text(`Year: ${currentYear}`);
 
+  updateChart(currentYear);
+
   function updateChart(year) {
     yearLabel.text(`Year: ${year}`);
 
-    const filtered = continents.map(c => ({
-      continent: c.continent,
-      value: c.values.find(v => v.Year === year)
-    })).filter(d => d.value);
+    const yearData = continents.map(c => {
+      const entry = c.values.find(v => v.Year === year);
+      return { continent: c.continent, Value: entry ? entry.Value : 0 };
+    });
 
-    const points = svg.selectAll(".year-dot")
-      .data(filtered, d => d.continent);
+    // JOIN
+    const bars = svg.selectAll(".bar").data(yearData, d => d.continent);
 
-    points.exit().remove();
+    bars.exit()
+      .transition()
+      .duration(500)
+      .attr("y", y(0))
+      .attr("height", height - y(0))
+      .remove();
 
-    points.enter()
-      .append("circle")
-      .attr("class", "year-dot")
-      .merge(points)
-      .attr("cx", d => x(d.value.Year))
-      .attr("cy", d => y(d.value.Value))
-      .attr("r", 5)
+    bars.enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", d => x(d.continent))
+      .attr("width", x.bandwidth())
       .attr("fill", d => color(d.continent))
+      .attr("y", y(0))
+      .attr("height", height - y(0))
+      .merge(bars)
+      .transition()
+      .duration(800)
+      .attr("y", d => y(d.Value))
+      .attr("height", d => height - y(d.Value));
+
+    svg.selectAll(".bar")
       .on("mouseover", (event, d) => {
         tooltip.transition().style("opacity", 1);
-        tooltip.html(`<b>${d.continent}</b><br>${year}: ${d.value.Value.toFixed(2)}°C`)
+        tooltip.html(`<b>${d.continent}</b><br>${year}: ${d.Value.toFixed(2)}°C`)
           .style("left", event.pageX + 8 + "px")
           .style("top", event.pageY - 28 + "px");
       })
       .on("mouseout", () => tooltip.transition().style("opacity", 0));
   }
-
-  updateChart(currentYear);
 });
